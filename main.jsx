0b7bf38fbd8c5ba5a027b4e31027ca2d46c528fe
hpvu <39536328+hpvu@users.noreply.github.com>
2025-05-29 17:46:42 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+ 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WeightTracker from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WeightTracker />
+  </React.StrictMode>
+);
